36d567e1d4bbae4fa1785c38facaa0db4891529d
LatoyaBogle <41763275+LatoyaBogle@users.noreply.github.com>
2018-11-27 18:29:45 -0500

    Add files via upload
    adding files

--- server.js
+++ server.js
@@ -1,48 +1,35 @@
-require("dotenv").config();
 var express = require("express");
 var bodyParser = require("body-parser");
-var exphbs = require("express-handlebars");
 
-var db = require("./models");
-
-var app = express();
 var PORT = process.env.PORT || 3000;
 
-// Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-// Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+// Import routes and give the server access to them.
+var routes = require("./controllers/fitbitController.js");
+var routes = require("./controllers/authenticate_controller.js");
 
-var syncOptions = { force: false };
+app.use(routes);
 
-// If running a test, set syncOptions.force to true
-// clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
-}
 
-// Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
+
+
+app.listen(PORT, function() {
+    console.log("App now listening at localhost:" + PORT);
   });
-});
-
-module.exports = app;
+  
